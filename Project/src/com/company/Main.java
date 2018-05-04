dc988580c8cef2969e7596993af319af8bd7ec3d
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-05-04 10:54:03 +0300

    metode deleteElev

--- Project/src/com/company/Main.java
+++ Project/src/com/company/Main.java
@@ -2,11 +2,13 @@ package com.company;
 
 import java.sql.SQLException;
 
-import static com.company.personnel.Elev.addElev;
+import static com.company.personnel.Elev.createElev;
+import static com.company.personnel.Elev.deleteElev;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-	addElev(22, "Hincu", "Mihaela", "12A");
+	createElev(22, "Hincu", "Mihaela", "12A");
+	deleteElev(22);
     }
 }
--- Project/src/com/company/personnel/Elev.java
+++ Project/src/com/company/personnel/Elev.java
@@ -8,10 +8,11 @@ import static com.company.database.DataBase.USER;
 
 public class Elev {
 
-    public static void addElev(int ID_ELEV, String nume,
+    private static Connection conn = null;
+    private static Statement stmt = null;
+
+    public static void createElev(int ID_ELEV, String nume,
                                String prenume, String nume_clasa) throws SQLException {
-        Connection conn = null;
-        Statement stmt = null;
         try{
             //STEP 2: Register JDBC driver
             Class.forName("com.mysql.jdbc.Driver");
@@ -30,6 +31,44 @@ public class Elev {
             stmt.executeUpdate(sql);
 
             System.out.println("Inserted record into the table...");
+        } catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            if(stmt!=null) {
+                conn.close();
+            }
+            try{
+                if(null != conn) {
+                    conn.close();
+                }
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+    }
+
+    public static void deleteElev(int ID_ELEV) throws SQLException {
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+
+            //STEP 4: Execute a query
+            System.out.println("Deleting record from the table...");
+
+            stmt = conn.createStatement();
+            String sql = "DELETE FROM elev " +
+                    "WHERE ID_ELEV = " + ID_ELEV;
+            stmt.executeUpdate(sql);
+
+            System.out.println("Deleted record from the table...");
         } catch(Exception e){
             //Handle errors for Class.forName
             e.printStackTrace();
