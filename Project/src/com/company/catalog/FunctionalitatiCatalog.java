6a146163d989e5c9740860e55acbaa8e2791dc7a
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-05-04 23:07:55 +0300

    adaugare parola

--- Project/src/com/company/catalog/FunctionalitatiCatalog.java
+++ Project/src/com/company/catalog/FunctionalitatiCatalog.java
@@ -10,19 +10,6 @@ public class FunctionalitatiCatalog {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         createConnection();
-
-        //createElev(22, "Hincu", "Mihaela", "12A");
-        //readElev();
-        //updateElev(22, "Marinela");
-        //deleteElev(22);
-
-        //adaugareNota(21, 2, 9, "15-04-2018");
-        //adaugareAbsenta(19, 2, "01-05-2018", "nemotivat");
-        //PROBLEME MODIFICA DAR NU SE MODIFICA IN BAZA DE DATE motivareAbsenta(18,1,"01-05-2018");
-
-        //situatieElev(22);
-
         login();
-
     }
 }
--- Project/src/com/company/catalog/LoginCatalog.java
+++ Project/src/com/company/catalog/LoginCatalog.java
@@ -14,31 +14,40 @@ public class LoginCatalog {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("enter username");
         String username = keyboard.next();
+        System.out.println("enter password");
+        String password = keyboard.next();
 
+        //vedem daca parola este corecta
         //vedem daca username-ul este de tip elev/profesor/director
         stmt = conn.createStatement();
-        String sql = "SELECT tip FROM login WHERE username = '" + username + "'";
+        String sql = "SELECT parola, tip FROM login WHERE username = '" + username + "'";
         ResultSet rs = stmt.executeQuery(sql);
         String tip = "";
+        String parola = "";
         while(rs.next()){
+            parola = rs.getString("parola");
             tip  = rs.getString("tip");
         }
         rs.close();
 
-        switch (tip) {
-            case "elev":
-                loginElev();
-                break;
-            case "profesor":
-                loginProfesor();
-                break;
-            case "director":
-                loginDirector();
-                break;
-            default:
-                System.out.println("-username inexistent-");
-                break;
+        //TREBUIE FACUT CEVA CA SA SE POATA INTRODUCE PAROLA PANA E CORECTA
+        if(parola.equals(password)) {
+            switch (tip) {
+                case "elev":
+                    loginElev();
+                    break;
+                case "profesor":
+                    loginProfesor();
+                    break;
+                case "director":
+                    loginDirector();
+                    break;
+                default:
+                    System.out.println("-username inexistent-");
+                    break;
+            }
         }
+        else System.out.println("-parola incorecta-");
     }
 
     private static void loginDirector() {
@@ -48,6 +57,11 @@ public class LoginCatalog {
         //aici o sa se poata accesa metodele din clasa Elev
 
         System.out.println("este director");
+
+        //createElev(22, "Hincu", "Mihaela", "12A");
+        //readElev();
+        //updateElev(22, "Marinela");
+        //deleteElev(22);
     }
 
     private static void loginProfesor() {
@@ -58,6 +72,10 @@ public class LoginCatalog {
         //aici o sa se poata acecsa metodele din clasa note si absente
 
         System.out.println("este profesor");
+
+        //adaugareNota(21, 2, 9, "15-04-2018");
+        //adaugareAbsenta(19, 2, "01-05-2018", "nemotivat");
+        //PROBLEME MODIFICA DAR NU SE MODIFICA IN BAZA DE DATE motivareAbsenta(18,1,"01-05-2018");
     }
 
     private static void loginElev() {
@@ -67,6 +85,8 @@ public class LoginCatalog {
         //aici o sa se poata accesa metodele din clasa rapoarte
 
         System.out.println("este elev");
+
+        //situatieElev(22);
         }
 
 
