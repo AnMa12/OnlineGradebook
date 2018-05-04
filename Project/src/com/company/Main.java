05573bfe1e445142fe57f30e3512ed761cdbf8cb
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-05-04 11:00:41 +0300

    update method

--- Project/src/com/company/Main.java
+++ Project/src/com/company/Main.java
@@ -4,11 +4,13 @@ import java.sql.SQLException;
 
 import static com.company.personnel.Elev.createElev;
 import static com.company.personnel.Elev.deleteElev;
+import static com.company.personnel.Elev.updateElev;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-	createElev(22, "Hincu", "Mihaela", "12A");
-	deleteElev(22);
+	//createElev(22, "Hincu", "Mihaela", "12A");
+	//deleteElev(22);
+        updateElev("Hincu", 22);
     }
 }
--- Project/src/com/company/personnel/Elev.java
+++ Project/src/com/company/personnel/Elev.java
@@ -50,6 +50,40 @@ public class Elev {
         System.out.println("Goodbye!");
     }
 
+    public static void updateElev(String numeNou, int ID_ELEV) throws SQLException {
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+
+            //STEP 4: Execute a query
+            System.out.println("Updating record from the table...");
+            stmt = conn.createStatement();
+            String sql = "UPDATE elev " +
+                    "SET nume = '" + numeNou + "' WHERE ID_ELEV = " + ID_ELEV;
+            stmt.executeUpdate(sql);
+            System.out.println("Updated record from the table...");
+        } catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            if(stmt!=null) conn.close();
+            try{
+                if(conn!=null) {
+                    conn.close();
+                }
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+    }
+
     public static void deleteElev(int ID_ELEV) throws SQLException {
         try{
             //STEP 2: Register JDBC driver
