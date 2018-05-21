fdcf44b6c14debfed4084a1681d7ece7bd2e7d05
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-05-21 23:26:04 +0300

    the party is over, go home
    termina si interfata elevi

--- Project/src/com/company/catalog/FunctionalitatiCatalog.java
+++ Project/src/com/company/catalog/FunctionalitatiCatalog.java
@@ -2,12 +2,18 @@ package com.company.catalog;
 import java.sql.SQLException;
 
 import static com.company.catalog.LoginCatalog.login;
+import static com.company.catalog.RapoarteCatalog.getClasaByIdElev;
+import static com.company.catalog.RapoarteCatalog.medieClasa;
 import static com.company.database.DataBaseLogin.createConnection;
+import static com.company.detaliiElev.Note.getMedieElevByID;
 
 public class FunctionalitatiCatalog {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         createConnection();
         login();
+
+
+
     }
 }
